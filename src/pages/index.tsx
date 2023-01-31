a1c236f6209c4e4937b8d30fb141dd4afebe0bf3
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2023-01-30 19:51:08 -0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home() {
         <title>Discord AutoMod Regexes</title>
         <meta
           name="description"
-          content="A list of regexes that can be used with Discord AutoMod"
+          content="A collection of regexes that can be used with Discord's AutoMod filters. This is not affiliated with Discord."
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
